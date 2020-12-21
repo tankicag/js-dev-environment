2a50200e4ce734100428d1c7830c3ff864494ed0
GTango <gonevatankica@gmail.com>
2020-12-21 10:19:13 +0000

    module 10 work in progres

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,18 +1,24 @@
-var express = require("express");
-var path = require("path");
-var open = require("open");
+import express from "express";
+import path from "path";
+import open from "open";
+import compression from "compression";
 
-var port = 3000;
-var app = express();
+/*eslint-disable no-console */
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../src/index.html"));
+const port = 3000;
+const app = express();
+
+app.use(express.static("dist"));
+app.use(compression());
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
-    open("http://localhost:" + port);
+    open(`http://localhost:${port}`);
   }
 });
